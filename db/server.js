9a16c1c843853f4c4dcf6c9ada1efac693d6a6fc
Marina Mustac <mm4331@rit.edu>
2025-01-13 16:03:57 +0100

    small changes

--- db/server.js
+++ db/server.js
@@ -11,3 +11,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log("JSON Server is running on port:", PORT);
 });
+
+console.log('Router:', router);
+console.log('Port:', PORT);
